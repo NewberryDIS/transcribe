fbd14d99f10eb6d995dde1664b92a7055053b181
N White <c2lkn.t@gmail.com>
2020-06-08 09:15:58 -0500

    switching to CRA

--- build/server.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const path = require('path');
-const app = express();
-
-app.use(express.static(path.join(__dirname, 'cranscribe/')));
-app.get('cranscribe/*', function(req, res) {
-  res.sendFile(path.join(__dirname, 'cranscribe/', 'index.html'));
-});
-app.listen(22799);
